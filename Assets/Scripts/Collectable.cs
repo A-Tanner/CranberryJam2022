885a9c9675c0137b94fab40e51b6fc2ecb2a47f6
Ray Tanner <atanner5@ewu.edu>
2022-12-03 19:17:17 -0800

    fix collectable animations, make control vars public

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    private float rps = 30f;
-    private float floatBound = 0.04f;
-    private float floatSpeed = 0.02f;
-    private float floatLocation = 0f;
+    public float rps = 50f;
+    public float floatDuration = 2f;
+    public float floatSpeed = 0.07f;
+    public float floatElapsed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,15 +26,12 @@ public class Collectable : MonoBehaviour
         this.transform.Rotate(new Vector3(0, rps * Time.deltaTime, 0));
         float floatDist = floatSpeed * Time.deltaTime;
         this.transform.Translate(new Vector3(0, floatDist, 0));
-        if (Mathf.Abs(floatLocation) >= floatBound)
+        floatElapsed += Time.deltaTime;
+        if(floatElapsed >= floatDuration)
         {
+            floatElapsed = 0;
             floatSpeed *= -1;
-            if (floatSpeed < 0) {
-                floatLocation = -floatBound;
-            } else
-            {
-                floatLocation = floatBound;
-            }
         }
+
     }
 }
