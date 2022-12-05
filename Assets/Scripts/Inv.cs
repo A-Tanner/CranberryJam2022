f3d52d11af36ec43bcd5411c2e79d88853f9ea6a
Ray Tanner <atanner5@ewu.edu>
2022-12-05 01:45:13 -0800

    Add revivify ray. It's working!

--- Assets/Scripts/Inv.cs
+++ Assets/Scripts/Inv.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Inv : MonoBehaviour
 {
     public int turkeyCount { get; private set; }
     public bool hasGun { get; private set; }
-    private List<KeyEnum> keys = new();
 
+    private List<KeyEnum> keys = new();
+    [SerializeField]
+    private GameObject gun;
+    [SerializeField]
+    private GameObject beam;
+    private void Update()
+    {
+        if (hasGun && Input.GetMouseButton(0))
+        {
+            gun.GetComponentInChildren<ParticleSystem>().Play();
+            beam.SetActive(true);
+        } else
+        {
+             beam.SetActive(false);
+        }
+    }
 
     public void takeTurkey()
     {
         turkeyCount++;
+
+        GameObject.FindGameObjectWithTag("hint").GetComponent<Text>().text = ($"Revived {turkeyCount} turkeys!");
     }
 
     public void takeKey(KeyEnum type)
@@ -23,11 +41,11 @@ public class Inv : MonoBehaviour
     public void takeGun()
     {
         hasGun = true;
+        gun.SetActive(true);
     }
 
     public bool checkKey(KeyEnum type)
     {
         return keys.Contains(type);
     }
-
 }
--- Assets/Scripts/Turkey.cs
+++ Assets/Scripts/Turkey.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Turkey : MonoBehaviour
 {
 
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("revivify"))
+        Debug.Log("contact");
+        if (other.CompareTag("ray"))
         {
+            gameObject.GetComponent<Animator>().SetBool("revived", true);
             GameObject.FindGameObjectWithTag("frank").GetComponent<Inv>().takeTurkey();
-            //Set up ai pathfinding
         }
     }
 }
